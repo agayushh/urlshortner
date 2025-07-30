0c86d6eb9a964711c573733d5f093d7b678347db
agayushh <agayush088@gmail.com>
2025-07-31 00:41:45 +0530

    Refactor server setup to include database connection and URL routing

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectToDB } from "./connectDb.js ";
+import { router } from "./routes/url.route.js";
 const app = express();
 
 dotenv.config({
   path: "./.env",
 });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT;
 
-app.listen(PORT || 8000, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+connectToDB()
+  .then(() => {
+    app.listen(PORT || 8000, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Something went wrong while connecting ${err}`);
+  });
+
+
+app.use("/url", router)
