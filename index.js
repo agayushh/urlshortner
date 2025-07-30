e306e33355a5bdc39eb4c4a46f86632d977123fb
agayushh <agayush088@gmail.com>
2025-07-30 19:34:41 +0530

    Set up Express server and add dotenv for environment variable management

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT || 8000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
