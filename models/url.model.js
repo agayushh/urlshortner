7624c6fb9db43041f0acd18ef056bfb3f7418b23
agayushh <agayush088@gmail.com>
2025-07-30 19:40:27 +0530

    Add URL model schema for short URL storage

---
+++ models/url.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    totalClicks: [
+      {
+        timestamps: { type: Number },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const URL = mongoose.model("url", userSchema);
