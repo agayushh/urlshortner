3ced64968f38e80a6011d3718b124f74c5e8fd2a
agayushh <agayush088@gmail.com>
2025-07-31 15:13:46 +0530

    fix: update URL handling to include visit tracking and redirect functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectToDB } from "./connectDb.js";
 import { urlRouter } from "./routes/url.route.js";
+import { URL } from "./models/url.model.js";
 const app = express();
 
 dotenv.config({
@@ -12,11 +13,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/url", urlRouter);
-const PORT = process.env.PORT;
-
-app.get("/test", (req, res) => {
-  res.send("hi there");
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
 });
+
+const PORT = process.env.PORT;
 
 connectToDB()
   .then(() => {
